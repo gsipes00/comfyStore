f244a8ca5c4d4bc2aeaa3e10e930347afc6b8e95
Gene Sipes <gsipes00@yahoo.com>
2022-10-05 16:49:57 -0500

    added cart sidebar open and close functionality in js

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,13 @@
 import { getElement } from "../utils.js";
-
-export const openCart = () => {};
+const cartOverlay = document.querySelector(".cart-overlay");
+const closeCartBtn = document.querySelector(".cart-close");
+const toggleCartBtn = document.querySelector(".toggle-cart");
+toggleCartBtn.addEventListener("click", function () {
+  cartOverlay.classList.add("show");
+});
+closeCartBtn.addEventListener("click", function () {
+  cartOverlay.classList.remove("show");
+});
+export const openCart = () => {
+  cartOverlay.classList.add("show");
+};
