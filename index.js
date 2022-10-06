03b330517969a6aecac8e92f94ce46f4655860db
Gene Sipes <gsipes00@yahoo.com>
2022-10-05 20:51:46 -0500

    destructured json data

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ import fetchProducts from "./src/fetchProducts.js";
 import { setupStore, store } from "./src/store.js";
 import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
+
 const init = async () => {
   const products = await fetchProducts();
-  console.log(products);
+  if (products) {
+    // add products to the store
+    setupStore(products);
+    console.log(store);
+  }
 };
 
 window.addEventListener("DOMContentLoaded", init);
--- src/store.js
+++ src/store.js
@@ -1,5 +1,21 @@
-import { getStorageItem, setStorageItem } from './utils.js';
+import { getStorageItem, setStorageItem } from "./utils.js";
+
 let store = [];
-const setupStore = () => {};
+const setupStore = (products) => {
+  store = products.map((product) => {
+    const {
+      id,
+      fields: { featured, name, price, company, colors, image: img },
+    } = product;
+    const image = img[0].thumbnails.large.url;
+    return { id, featured, name, price, company, colors, image };
+  });
+};
+console.log(store);
 const findProduct = () => {};
 export { store, setupStore, findProduct };
+
+// video 416 Setup Store ~9min explains:
+// 1) How to destructure the nested json response as depicted in the code above
+// 2) How console logging the store variable return a [], since the setupStore function
+// is being called after the console log for store in this script.
